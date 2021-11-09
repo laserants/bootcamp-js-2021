8816b1303d387280f255e1dd0010fafb20805b68
Miguel Roman <miguelerm@gmail.com>
2021-11-09 01:16:05 -0600

    cambio de texto y let a const

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let paragraphs = document.getElementsByTagName("p");
-console.log(paragraphs.length);
+const paragraphs = document.getElementsByTagName("p");
+console.log("parrafos en el documento: ", paragraphs.length);
 
 if (paragraphs.length > 0)
 {
-    let paragraph = paragraphs[0];
+    const paragraph = paragraphs[0];
     paragraph.innerText = "Bienvenidos al bootcamp!";
 }
