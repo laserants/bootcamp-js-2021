47b8756020261b3046f33ff9ddfdd36865b35527
Miguel Roman <miguelerm@gmail.com>
2021-11-09 01:40:15 -0600

    se modifica el segundo parrafo

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const paragraphs = document.getElementsByTagName("p");
-console.log("parrafos en el documento: ", paragraphs.length);
 
 if (paragraphs.length > 0)
 {
     const paragraph = paragraphs[0];
     paragraph.innerText = "Bienvenidos al bootcamp!";
+}
+
+if (paragraphs.length > 1)
+{
+    const paragraph = paragraphs[1];
+    const fecha = new Date();
+    paragraph.innerText = "parrafos en el documento: " + paragraphs.length + " (" + fecha + ")";
 }
