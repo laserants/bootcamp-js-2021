75753433130a5b16ef76f1543f6cb9b3b5a8d474
Miguel Roman <miguelerm@gmail.com>
2021-11-11 03:23:51 -0600

    se agregan productos a la tabla

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const paragraphs = document.getElementsByTagName("p");
+const form = document.getElementsByTagName("form")[0];
+const tbody = document.getElementsByTagName("tbody")[0];
 
-if (paragraphs.length > 0)
-{
-    const paragraph = paragraphs[0];
-    paragraph.innerText = "Bienvenidos al bootcamp!";
-}
+form.addEventListener("submit", onSubmit);
 
-if (paragraphs.length > 1)
+/**
+ * 
+ * @param {Event} event 
+ */
+function onSubmit(event)
 {
-    const paragraph = paragraphs[1];
-    const fecha = new Date();
-    paragraph.innerText = "parrafos en el documento: " + paragraphs.length + " (" + fecha + ")";
+    event.preventDefault();
+
+    const data = new FormData(form);
+    const values = Array.from(data.entries());
+
+    const nombre = values[0][1];
+    const cantidad = values[1][1];
+    const precio = values[2][1];
+    const categoria = values[3][1];
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = "<td>X</td><td>" 
+        + nombre + "</td><td>" 
+        + cantidad + "</td><td>" 
+        + precio + "</td><td>" 
+        + 0 + '</td><td><a href="#">Editar</a> | <a href="#">Eliminar</a></td>';
+    tbody.appendChild(tr);
+
+    console.log(nombre);
+    console.log(cantidad);
+    console.log(precio);
+    console.log(categoria);
+
 }
