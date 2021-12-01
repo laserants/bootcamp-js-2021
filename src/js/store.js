910b2b7d58d32fbb3ed01569afb85929158e27a3
Miguel Roman <miguelerm@gmail.com>
2021-11-30 22:31:44 -0600

    15-npm-webpack-modules

--- src/js/store.js
+++ src/js/store.js
@@ -6,7 +6,7 @@ const ActionTypes = {
     ProductoAgregadoModificado: "producto-agregado-o-modificado",
 };
 
-const reducer = (state, action) => {
+export const reducer = (state, action) => {
 
     switch (action.type) {
         case ActionTypes.ProductoAgregado:
@@ -26,29 +26,29 @@ const reducer = (state, action) => {
     }
 };
 
-const productoSeleccionado = (codigo) => ({
+export const productoSeleccionado = (codigo) => ({
     type: ActionTypes.ProductoSeleccionado,
     payload: {
         codigo
     }
 });
 
-const productoEliminado = (codigo) => ({
+export const productoEliminado = (codigo) => ({
     type: ActionTypes.ProductoEliminado,
     payload: { codigo }
 });
 
-const productoModificado = (payload) => ({
+export const productoModificado = (payload) => ({
     type: ActionTypes.ProductoModificado,
     payload
 });
 
-const productoAgregado = (payload) => ({
+export const productoAgregado = (payload) => ({
     type: ActionTypes.ProductoAgregado,
     payload
 });
 
-const agregarOModificarProducto = (payload) => ({
+export const agregarOModificarProducto = (payload) => ({
     type: ActionTypes.ProductoAgregadoModificado,
     payload
 });
@@ -64,14 +64,14 @@ const agregarOModificarProducto = (payload) => ({
 //     }
 // }
 
-const loggerMiddleware = store => next => action => {
+export const loggerMiddleware = store => next => action => {
     console.log("dispatching", action);
     const result = next(action);
     console.log("next state", store.getState());
     return result;
 }
 
-const agregarOModificarProductoMiddleware = store => next => action => {
+export const agregarOModificarProductoMiddleware = store => next => action => {
     if (action.type != ActionTypes.ProductoAgregadoModificado)
     {
         return next(action);
@@ -132,7 +132,7 @@ function productoAgregadoReducer(state, action) {
     };
 }
 
-function generadorCodigoProductoBuilder(codigoInicial)
+export function generadorCodigoProductoBuilder(codigoInicial)
 {
     let codigo = codigoInicial;
     return store => next => action => {
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,4 +1,4 @@
-const ui = {
+export const ui = {
     onFormSubmit: (data) => {},
     onEliminarClick: (codigo) => {},
     onEditarClick: (codigo) => {},
