8e1b51f6ff35ff358ce30fdff5603b46db992b45
Miguel Roman <miguelerm@gmail.com>
2021-12-27 22:49:03 -0600

    24 http requests desde el cliente

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
 import App from "./app";
 import store from "./store";
 
@@ -10,3 +11,20 @@ ReactDOM.render(
         <App />
     </Provider>,
     rootElement);
+
+test();
+
+async function test()
+{
+    try {
+        console.log("antes de fetch");
+        const response = await axios.get('http://localhost:5001/producto');
+        const productos = response.data;
+        console.log("productos: ", productos);
+        console.log("despues de fetch: ", productos);
+    } catch (error) {
+        console.error("error en el request: ", error);
+    }
+}
+
+
