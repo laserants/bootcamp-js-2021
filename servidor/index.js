d6432b3449f5ecec3196d4387fa49b9157ba3deb
Miguel Roman <miguelerm@gmail.com>
2021-12-27 22:04:51 -0600

    23-backend con express.

--- servidor/index.js
+++ servidor/index.js
@@ -1,17 +1,36 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 
-let lastId = 0;
-const productos = [];
+let lastId = 1;
+let productos = [
+    {
+        nombre: "producto a",
+        cantidad: 1,
+        precio: 10,
+        codigo: lastId
+    }
+];
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(logs);
 
 app.get("/", (req, res) => res.send("<h1>API de productos</h1>"));
 
-app.get("/productos", (req, res) => res.json(productos));
+app.get("/productos", (req, res) => {
+    const filtro = req.query.filtro;
+
+    if (filtro)
+    {
+        res.json(productos.filter(p => p.nombre.indexOf(filtro) >= 0))
+    } else {
+        res.json(productos)
+    }
+});
+
 app.post("/productos", (req, res) => {
     lastId++;
     const producto = {...req.body, codigo: lastId };
@@ -20,8 +39,54 @@ app.post("/productos", (req, res) => {
     res.json(producto);
 })
 
-app.listen(5000, () => {
-    console.log("servidor express escuchando en puerto 5000")
+app.get("/productos/:codigo", (req, res) => {
+    const codigo = parseInt(req.params.codigo, 10);
+    const producto = productos.find(p => p.codigo == codigo);
+
+    if (!producto)
+    {
+        res.status(404)
+        res.json({ mensaje: "No existe ningun producto con codigo " + codigo });
+    } else {
+        res.status(200);
+        res.json(producto);
+    }
+})
+
+app.put("/productos/:codigo", (req, res) => {
+    const codigo = parseInt(req.params.codigo, 10);
+    const producto = productos.find(p => p.codigo == codigo);
+
+    if (!producto)
+    {
+        res.status(404)
+        res.json({ mensaje: "No existe ningun producto con codigo " + codigo });
+    } else {
+        const index = productos.indexOf(producto);
+        const nuevoProducto = productos[index] = { ...req.body, codigo };
+        res.status(200);
+        res.json(nuevoProducto);
+    }
+})
+
+
+app.delete("/productos/:codigo", (req, res) => {
+    const codigo = parseInt(req.params.codigo, 10);
+    const producto = productos.find(p => p.codigo == codigo);
+
+    if (!producto)
+    {
+        res.status(404)
+        res.json({ mensaje: "No existe ningun producto con codigo " + codigo });
+    } else {
+        productos = productos.filter(x => x != producto);
+        res.status(200);
+        res.json({ message: "Producto eliminado" });
+    }
+})
+
+app.listen(5001, () => {
+    console.log("servidor express escuchando en puerto 5001")
 })
 
 // function isAuthenticated(req, res, next) {
